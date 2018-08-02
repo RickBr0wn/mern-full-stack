7b223d714a02945d5a7337e82d7a6d1d03d26f81
RiCK BROWN <ricky.brown.00@gmail.com>
2018-08-02 22:52:02 +0100

    Added dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+import DataWindow from './components/DataWindow'
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">DS18b20 Temperature Data</h1>
         </header>
+        <DataWindow />
       </div>
     )
   }
