07096fdc60d9b2451fb1a8b11b5ab4170b32c415
RiCK BROWN <ricky.brown.00@gmail.com>
2018-08-03 12:09:28 +0100

    Fetch working

--- client/src/components/DataWindow.js
+++ client/src/components/DataWindow.js
@@ -1,27 +1,49 @@
 import React from 'react'
 
-import dummyData from './dummyData'
-
 class DataWindow extends React.Component {
-  state = {
-    items: dummyData
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      error: null,
+      isLoaded: false
+    }
   }
 
-  render(){
-    const {items} = this.state
-
-    return (
-      <div>
-        <h1>Data Window</h1>
-        {
-          items.map(({id, temp, time}) => (
-            <div key={id}>
-              {temp}{time}
-            </div>
-          ))
-        }
-      </div>
+  fetchAPI = () => {
+    fetch('/api/items', { mode: 'no-cors' })
+      .then(result => result.json())
+      .then(response => 
+        this.setState({
+          isLoaded: true,
+          data: response
+        }),
+      error => 
+        this.setState({
+          isLoaded: true,
+          error
+        })
     )
+  }
+
+  componentDidMount() {
+    this.fetchAPI()
+  }
+  
+  render(){
+    if(this.state.isLoaded) {
+      const {data} = this.state
+      console.log(data)
+      return (
+        <div>
+          <h1>Data Window</h1>
+        </div>
+      )
+    }else{
+      return (
+        <h1>Loading Data from API</h1>
+      )
+    }
   }
 }
 
